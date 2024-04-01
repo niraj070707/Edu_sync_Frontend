9739f0b1cc335a04849c857f343350f807fe5213
niraj070707 <karandeniraj28@gmail.com>
2024-04-01 15:30:06 +0530

    Add Authentication for Admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,46 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useInRouterContext } from "react-router-dom";
 import './App.css'
-import AdminLogin from "./Components/Login/AdminLogin";
+import { useCon } from "./UserContext"
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+import Login from "./Components/Login/Login";
+
+// Admin
+import AdminLogin from "./Components/Login/AdminLogin/AdminLogin";
+import Dashboard from "./Components/Admin/Dashboard";
+
+// Faculty
+import FacultyLogin from "./Components/Login/FacultyLogin/FacultyLogin";
+
+// Student
+import StudentLogin from "./Components/Login/StudentLogin/StudentLogin";
+
 
 function App() {
-  const user = {
-    user_type: "Admin"
-  }
+  axios.defaults.baseURL = "http://localhost:8080";
+  axios.defaults.withCredentials = true;
+  
+  const { User } = useCon();
+  
+  console.log("App_User", User);
   return (
       <>    
           <Routes>
-              <Route exact path="/login/Adminlogin" element={user.user_type==="Admin" ? <AdminLogin /> : <InvalidPage/>} />
+              {/* Home */}
+              <Route exact path="/" element={<Login />} />
+
+              {/* Admin */}
+              <Route exact path="/login/adminlogin" element={User && User.username=="admin" ?  <Navigate to="/admin/dashboard" /> : <AdminLogin />} />
+              <Route exact path="/admin/dashboard" element={User && User.username=="admin"? <Dashboard/> : <Navigate to="/login/adminlogin" /> } />
+
+              {/* Faculty */}
+              <Route path="/login/facultylogin" element={<FacultyLogin />} />
+
+
+              {/* Student */}
+              <Route path="/login/studentlogin" element={<StudentLogin />} />
+
           </Routes>
       </>
   )
--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,14 +1,36 @@
-import axios from "axios";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({children}){
-    const User = localStorage.getItem('User');
+export const UserContextProvider = ({ children }) => {
+    const [User, setUser] = useState({});
+
+    const storeUserInLS = (User)=>{
+        return localStorage.setItem("User", JSON.stringify(User))
+    }
+
+    useEffect(() => {
+        // Retrieve user data from localStorage when component mounts
+        const storedUser = localStorage.getItem('User');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    }, []);
+
+    console.log("Context User", User);
+
     return (
-        <UserContext.Provider value={User}>
+        <UserContext.Provider value={{ User, storeUserInLS }}>
             {children}
         </UserContext.Provider>
     );
 }
 
+export const useCon = ()=>{
+    const userContextValue = useContext(UserContext);
+    if (!userContextValue) {
+        throw new Error("useCon used outside of the Provider");
+    }
+    return userContextValue;    
+}
